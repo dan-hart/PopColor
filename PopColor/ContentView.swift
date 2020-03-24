533e09334f847426d34525f8d35d39fb2d88d5fa
Dan Hart <dhart94@gmail.com>
2020-03-24 08:00:05 -0500

    Initial Commit

---
+++ PopColor/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//
+//  ContentView
+//  PopColor
+//
+//  Created by Dan Hart on 3/24/20.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
